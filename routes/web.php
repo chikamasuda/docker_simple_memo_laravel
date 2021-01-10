1658ec2a67ef45ee6e08ad91d970b2d78bc0f6c7
chikamasuda <c.m.062820@gmail.com>
2021-01-10 11:04:25 +0900

    ユーザー登録機能を実装

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login.index');
+Route::post('/user/register', 'App\Http\Controllers\Auth\RegisterController@register')->name('user.exec.register');
 Route::get('/user', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('user.register');
 Route::get('/memo',function(){
     return view("memo");
 })->name('memo.index');
-
-Auth::routes();
-
