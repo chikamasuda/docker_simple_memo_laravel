5921df12048c786a659fa9a056e0d32a6d3a88fb
chikamasuda <c.m.062820@gmail.com>
2021-01-11 23:26:59 +0900

    ログイン情報を考慮した画面遷移を実装

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login.index');
 Route::post('/user/register', 'App\Http\Controllers\Auth\RegisterController@register')->name('user.exec.register');
 Route::get('/user', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('user.register');
+
+Route::group(['middleware' =>['auth']], function(){
 Route::get('/memo',function(){
     return view("memo");
 })->name('memo.index');
+});
 
 Auth::routes();
 
