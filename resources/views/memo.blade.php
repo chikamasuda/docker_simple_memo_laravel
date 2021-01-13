bab07df37df65b191f825a382aeaef29eb203dd9
chikamasuda <c.m.062820@gmail.com>
2021-01-14 02:46:28 +0900

    メモの更新機能を実装

--- resources/views/memo.blade.php
+++ resources/views/memo.blade.php
@@ -40,8 +40,8 @@
         @csrf
         <input type="hidden" name="edit_id" value="{{ $select_memo->id }}">
         <div id="memo-menu">
-          <button type="submit" class="btn btn-danger" formaction=""><i class="fas fa-trash-alt"></i></button>
-          <button type="submit" class="btn btn-success" formation=""><i class="fas fa-save"></i></button>
+          <button type="submit" class="btn btn-danger" formaction=""><i class="fas fa-trash-alt" formaction=""></i></button>
+          <button type="submit" class="btn btn-success" formaction="{{ route('memo.update') }}"><i class="fas fa-save"></i></button>
         </div>
         <input type="text" id="memo-title" name="edit_title" placeholder="タイトルを入力する..." value="{{ $select_memo->title }}">
         <textarea name="edit_content" id="memo-content" placeholder="内容を入力する...">{{ $select_memo->content }}</textarea>
--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,12 @@ Route::get('/', [LoginController::class, 'showLoginForm'])->name('login.index');
 Route::get('/user', [RegisterController::class, 'showRegistrationForm'])->name('user.register');
 Route::post('/user/register', [RegisterController::class, 'register'])->name('user.exec.register');
 
-Route::group(['middleware' =>['auth']], function(){
-Route::get('/memo',[MemoController::class, 'index'])->name('memo.index');
-Route::get('/memo/add',[MemoController::class, 'add'])->name('memo.add');
-Route::get('logout', [LoginController::class, 'logout'])->name('memo.logout');
-Route::get('/memo/select', [MemoController::class, 'select'])->name('memo.select');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/memo', [MemoController::class, 'index'])->name('memo.index');
+    Route::get('/memo/add', [MemoController::class, 'add'])->name('memo.add');
+    Route::get('logout', [LoginController::class, 'logout'])->name('memo.logout');
+    Route::get('/memo/select', [MemoController::class, 'select'])->name('memo.select');
+    Route::post('/memo/update', [MemoController::class, 'update'])->name('memo.update');
 });
 
-Auth::routes();
 
